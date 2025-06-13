0185d667d3142a35fd04d67aaef56186e1daa900
IsaGarciaSense <isa@sense-ai.co>
2025-06-13 17:00:19 -0500

    "not yet"

--- test/MyBLEClient.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "esp_bt.h"
-#include "esp_gap_ble_api.h"
-
-class MyBLEClient {
-public:
-    static void init();                          // Inicializa BLE
-    static void startScan(unsigned long duration);    // Inicia escaneo BLE
-};
